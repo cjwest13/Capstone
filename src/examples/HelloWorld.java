9501121814cd89c7efc91ae711496ca4af4de11d
unknown <John@JBurrell.SJVINALLC.LOCAL>
2015-09-17 11:26:44 -0400

    added a hello world file to test intellij and integration with git

---
+++ src/examples/HelloWorld.java
@@ -0,0 +1,11 @@
+package examples;
+
+/**
+ * Used for testing the IDE
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
