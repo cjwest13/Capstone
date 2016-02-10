69c83024f5af48732ae841a006c0711355caf684
Clifton West <cwest2@catamount.wcu.edu>
2016-02-10 15:26:04 -0500

    new new stuff

---
+++ src/utilities/Keyboard.java
@@ -0,0 +1,21 @@
+package utilities;
+
+/**
+ * API for providing system data.
+ * @author  Clifton West
+ * @version February 6, 2016
+ */
+public class Keyboard {
+
+    /** Make String 2D Array for normal QWERTY keyboard */
+    static final String[][] QWERTY = new String[][] {
+            {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p"},
+            {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"},
+            {"z", "x", "c", "v", "b", "n", "m", ",", ".", "/"},
+    };
+
+    //edit keyboard above, make different styles of the keyboard
+    //add key codes
+    //pray to god that it works somehow
+
+}
